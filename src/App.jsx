1d5f6713c2b80268ad8778d62ddfa623322470b4
Aake Hauge <aakesh@stud.ntnu.no>
2023-01-26 13:51:23 +0100

    naa

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <div className="App">
       <p>Heisann</p>
-      <div id="styledimg"></div>
+      <img src='https://www.google.com/url?sa=i&url=https%3A%2F%2Fopen.spotify.com%2Falbum%2F7EBD5y4260xQm39iROuqnE&psig=AOvVaw05D-BiNuBnS3RP7ZAcGzP3&ust=1674823790118000&source=images&cd=vfe&ved=0CBAQjRxqFwoTCODf8Pqi5fwCFQAAAAAdAAAAABAD'></img>
     </div>
   )
 }
